bb49bac41b1ead90a811e154704c1969931a3ee8
Josue Vargas Zúñiga <jovz1798@gmail.com>
2022-05-16 21:55:49 +0000

    Excuses generator app

--- src/app.js
+++ src/app.js
@@ -5,7 +5,36 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
+const excuses = {
+  who: ["The dog", "My grandma", "His turtle", "My bird"],
+  action: ["ate", "peed", "crushed", "broke"],
+  what: ["my homework", "the keys", "the car"],
+  when: [
+    "before the class",
+    "right on time",
+    "when I finished",
+    "during my lunch",
+    "while I was praying"
+  ]
+};
+const random = arr => {
+  return arr[Math.floor(Math.random() * arr.length)];
+};
+
+const excuseGenerator = excuses => {
+  return `${random(excuses.who)} ${random(excuses.action)} ${random(
+    excuses.what
+  )} ${random(excuses.when)}!`;
+};
+
+const excuse = document.querySelector("#excuse");
+const btn = document.querySelector("#generate");
+
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  excuse.innerHTML = excuseGenerator(excuses);
 };
+
+btn.addEventListener("click", () => {
+  excuse.innerHTML = excuseGenerator(excuses);
+});
